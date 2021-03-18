bda640537fa6c73a8d624825a65ff853f9ec67ce
biermapa <biermapa@PLRI02>
2021-03-18 15:50:56 +0100

    added some images and a basic readme for the aql descriptions

--- SmICSCoreLib/AQL/PatientInformation/Patient_Labordaten/LabDataModel.cs
+++ SmICSCoreLib/AQL/PatientInformation/Patient_Labordaten/LabDataModel.cs
@@ -24,8 +24,8 @@ namespace SmICSCoreLib.AQL.PatientInformation.Patient_Labordaten
         public DateTime ZeitpunktProbeneingang { get; set; }
         [JsonProperty(PropertyName = "Probenart")]
         public string MaterialID { get; set; } = "Abs";
-        [JsonProperty(PropertyName = "Screening")]
-        public bool screening { get; set; } = false;
+        [JsonProperty(PropertyName = "Screening")]  //Nur für Visualisierung -> nicht essentiell
+        public bool screening { get; set; } = false; //Nur für Visualisierung -> nicht essentiell
         [JsonProperty(PropertyName = "Material_l")]
         [DefaultValue(" ")]
         public string Material_l { get; set; }
