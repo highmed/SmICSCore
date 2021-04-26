cc73432cd6f9c2bd2c6112e1b3ad70c2d384e680
Emsibil <biermann.pascal@gmx.de>
2021-04-26 14:41:37 +0200

    Changed Images, Added StartupText Added Dynamic Link

--- SmICSWebApp/Startup.cs
+++ SmICSWebApp/Startup.cs
@@ -42,6 +42,7 @@ namespace SmICSWebApp
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
