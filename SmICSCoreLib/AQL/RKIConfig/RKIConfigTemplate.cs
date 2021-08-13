b17162d74483e9888d2043d601cdbd177f6b7268
biermapa <biermann.pascal@mh-hannover.de>
2021-08-13 14:31:35 +0200

    Change from Pascal RKIConfig

--- SmICSCoreLib/AQL/RKIConfig/RKIConfigTemplate.cs
+++ SmICSCoreLib/AQL/RKIConfig/RKIConfigTemplate.cs
@@ -7,22 +7,17 @@ namespace SmICSCoreLib.AQL.RKIConfig
 {
     public class RKIConfigTemplate
     {
-        //[Required]
-        //public string Station { get; set; }
-        //[Required]
-        //public string Erreger { get; set; }
-        //[Required]
-        //public string Zeitraum { get; set; }
-        //public bool Retro { get; set; } = false;
+        [Required]
+        public string Station { get; set; }
+        [Required]
+        public string Erreger { get; set; }
+        [Required]
+        public string Zeitraum { get; set; }
+        public bool Retro { get; set; } = false;
         [Required]
         public string Zeitpunkt { get; set; }
 
-        public List<RKIConfigRulesTemplate> RKIConfigFormAdd { get; set; }
 
-        public RKIConfigTemplate()
-        {
-            RKIConfigFormAdd = new List<RKIConfigRulesTemplate>();
-        }
 
     }
 }
