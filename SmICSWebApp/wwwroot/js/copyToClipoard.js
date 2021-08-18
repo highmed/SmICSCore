a189cb3e17ce348accddbc8deee503bb5bac6860
Alan Yussef <yussefalhaji.alan@mh-hannover.de>
2021-08-18 13:59:47 +0200

    Add a Script to copy PatientenID To Clipboard

---
+++ SmICSWebApp/wwwroot/js/copyToClipoard.js
@@ -0,0 +1,18 @@
+function copyToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
