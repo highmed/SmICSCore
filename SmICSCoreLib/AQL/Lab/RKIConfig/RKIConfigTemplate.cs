1a360f15f99b8847341af8193175aa5023dac071
schickkr <schickkr@PC3H00821>
2021-09-16 13:12:06 +0200

    Config Erneuerungen

--- SmICSCoreLib/AQL/Lab/RKIConfig/RKIConfigTemplate.cs
+++ SmICSCoreLib/AQL/Lab/RKIConfig/RKIConfigTemplate.cs
@@ -17,10 +17,11 @@ namespace SmICSCoreLib.AQL.Lab.RKIConfig
         [Required]
         public string Erreger { get; set; }
         [Required]
-        public string Zeitraum { get; set; }
+        public int Zeitraum { get; set; }
         public bool Retro { get; set; } = false;
         [Required]
         public string Erstellungsdatum { get; set; } = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'");
+        public string Erregerstatus { get; set; }
 
     }
 }
