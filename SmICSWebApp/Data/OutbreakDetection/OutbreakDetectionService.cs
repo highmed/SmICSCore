736d0868c63049bd1b9bb5f7d4fdf98b8a3febe7
biermapa <biermann.pascal@mh-hannover.de>
2021-11-04 11:17:02 +0100

    fixed bug if no RConfig or R results exists

--- SmICSWebApp/Data/OutbreakDetection/OutbreakDetectionService.cs
+++ SmICSWebApp/Data/OutbreakDetection/OutbreakDetectionService.cs
@@ -12,31 +12,56 @@ namespace SmICSWebApp.Data.OutbreakDetection
         private readonly string dir = @"./Resources/OutbreakDetection";
         public List<OutbreakDetectionConfig> GetConfigurations()
         {
-            List<OutbreakDetectionConfig> configs = new List<OutbreakDetectionConfig>();
-            foreach(string folder in Directory.GetDirectories(dir))
+            try
             {
-                string name = folder.Substring(dir.Length + 1);
-                string ward = name.Split("_")[1];
-                configs.Add(new OutbreakDetectionConfig { Name = name, Ward = ward});
+                List<OutbreakDetectionConfig> configs = new List<OutbreakDetectionConfig>();
+                foreach (string folder in Directory.GetDirectories(dir))
+                {
+                    string name = folder.Substring(dir.Length + 1);
+                    string ward = name.Split("_")[1];
+                    configs.Add(new OutbreakDetectionConfig { Name = name, Ward = ward });
+                }
+                return configs;
             }
-            return configs;
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
         }
 
         public OutbreakDetectionStoringModel GetLatestResult(OutbreakSaving outbreak)
         {
-            DirectoryInfo directory = new DirectoryInfo(dir + outbreak.ConfigName);
-            FileInfo file = directory.GetFiles().OrderByDescending(f => f.Name).FirstOrDefault();
-            return JSONReader<OutbreakDetectionStoringModel>.NewtonsoftReadSingle(file.FullName);
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(dir + outbreak.ConfigName);
+                FileInfo file = directory.GetFiles().OrderByDescending(f => f.Name).FirstOrDefault();
+                return JSONReader<OutbreakDetectionStoringModel>.NewtonsoftReadSingle(file.FullName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            
         }
 
         public List<OutbreakDetectionStoringModel> GetsResultsInTimespan(OutbreakSavingInTimespan outbreak)
         {
-            List<OutbreakDetectionStoringModel> OutbreakDetectionResults = new List<OutbreakDetectionStoringModel>();
-            for (DateTime date = outbreak.Starttime; date <= outbreak.Endtime; date = date.AddDays(1.0))
+            try
             {
-                OutbreakDetectionResults.Add(JSONReader<OutbreakDetectionStoringModel>.NewtonsoftReadSingle(dir + "/" + outbreak.ConfigName + "/" + date.ToString("yyyy-MM-dd") + ".json"));
+                List<OutbreakDetectionStoringModel> OutbreakDetectionResults = new List<OutbreakDetectionStoringModel>();
+                for (DateTime date = outbreak.Starttime; date <= outbreak.Endtime; date = date.AddDays(1.0))
+                {
+                    OutbreakDetectionResults.Add(JSONReader<OutbreakDetectionStoringModel>.NewtonsoftReadSingle(dir + "/" + outbreak.ConfigName + "/" + date.ToString("yyyy-MM-dd") + ".json"));
+                }
+                return OutbreakDetectionResults;
             }
-            return OutbreakDetectionResults;
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
         }
     }
 }
