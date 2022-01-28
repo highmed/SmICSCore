8585666b13136080c153a8da1504e9c1c18d547b
biermapa <biermapa@PLRI02>
2022-01-28 09:05:44 +0100

    added functional filter to contact

---
+++ SmICSWebApp/Data/Contact/FilterParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmICSWebApp.Data.Contact
+{
+    public class FilterParameter
+    {
+        public string ResistenceDetail { get; set; }
+        public string ContactDetail { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public List<string> ResistanceOptions { get; internal set; } = new List<string> { "Alles" };
+        public List<string> ContactOptions { get; } = new List<string> { "Alles", "Stationskontakte", "Zimmerkontakte" };
+
+    }
+}
