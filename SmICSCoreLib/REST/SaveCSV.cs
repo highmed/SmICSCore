b7df8e4479b088615046fe59112a7738716e6655
schickkr <schickkr@PC3H00821>
2022-05-13 13:36:10 +0200

    First try for average

---
+++ SmICSCoreLib/REST/SaveCSV.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+
+namespace SmICSCoreLib.REST
+{
+    public static class SaveCSV
+    {
+        public static void ToCsv<T>(List<T> reportData, string path)
+        {
+            var lines = new List<string>();
+            IEnumerable<PropertyDescriptor> props = TypeDescriptor.GetProperties(typeof(T)).OfType<PropertyDescriptor>();
+            var header = string.Join(",", props.ToList().Select(x => x.Name));
+            lines.Add(header);
+            var valueLines = reportData.Select(row => string.Join(",", header.Split(',').Select(a => row.GetType().GetProperty(a).GetValue(row, null))));
+            lines.AddRange(valueLines);
+            File.WriteAllLines(path, lines.ToArray());
+        }
+    }
+}
